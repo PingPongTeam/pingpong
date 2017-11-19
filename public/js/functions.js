48c42ff75a0c4aae3d75510da4083f7fcfc7185c
Olle Sandberg <olle.sandberg@dinbox.se>
2017-11-19 16:34:54 +0100

    Create stupid tabs.

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,10 +1,12 @@
+// Load default tab
+document.getElementById("defaultOpen").click();
+
+// Setup websocket
 var host = "ws://" + window.location.hostname + ":8080";
 var ws = new WebSocket(host);
-ws.onopen = function() {
-    console.log("Connected");
-};
+ws.onopen = function() { console.log("Connected"); };
 
-ws.onmessage = function (evt) { 
+ws.onmessage = function(evt) {
     var obj = JSON.parse(evt.data);
     console.log("Message is received: " + JSON.stringify(obj));
     if (obj.type === "players") {
@@ -12,22 +14,34 @@ ws.onmessage = function (evt) {
     }
 };
 
-ws.onclose = function() { 
-    console.log("Connection closed"); 
-};
+ws.onclose = function() { console.log("Connection closed"); };
+window.onbeforeunload = function(event) { ws.close(); };
 
-window.onbeforeunload = function(event) {
-    ws.close();
-};
-    
-function updatePlayers(players) {
+function updatePlayers(players)
+{
     players.sort(function(p1, p2) { return p1.score <= p2.score; });
     document.getElementById("players").innerHTML =
         "<tr><th>Position</th><th>Name</th><th>Score</th><tr>";
     for (var i = 0; i < players.length; i++) {
         document.getElementById("players").innerHTML +=
-            "<tr><th>" + (i + 1) + "</th><th>" + players[i].name +
-            "</th><th>" + +players[i].score + "</th><tr>";
+            "<tr><th>" + (i + 1) + "</th><th>" + players[i].name + "</th><th>" +
+            +players[i].score + "</th><tr>";
     }
 }
 
+function openTab(evt, tabName)
+{
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
