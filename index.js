6d6fac444bcab26c31934109b42ec063b2c9ab8f
Olle Sandberg <olle.sandberg@dinbox.se>
2017-12-04 22:52:52 +0100

    Some user signup stuff.

--- index.js
+++ index.js
@@ -9,31 +9,46 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const config = require('./config.js');
 const r = require('rethinkdbdash')(config.rethink);
+const jwt = require('jsonwebtoken');
 const common = require('./common.js');
 const log = common.log;
 
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-r.table('users').changes().run().then(function(cursor) {
-    cursor.each(function(err, item) {
-        log("User table updated: " + JSON.stringify(item));
-    });
+
+io.on('connection', function onConnection(socket) {
+
+  log("New connection from " + socket.request.connection.remoteAddress);
+
+  socket.on('user:signup', function onUserSignup(data, answer) {
+    log("User signup: " + JSON.stringify(data));
+    r.table('users')
+        .insert(
+            {userId : data.email, name : data.name, password : data.password})
+        .run()
+        .then(function onCreatedUser(result) {
+          if (result.errors) {
+            log("Error creating user: " + JSON.stringify(result));
+            // Leaking error message contains all user information.
+            // Rethinkdb seems to return very machine-undfriendly error
+            // messages.
+            // TODO: Should return informative error messsage.
+            answer({status : 1, errors : [ {msg : result.first_error} ]});
+          } else {
+            log("New user created: " + JSON.stringify(result));
+            // TODO: Should fetch/generate and return a jwt.
+            answer({status : 0, jwt : "Made up JWT"});
+          }
+        });
+  });
+
+  socket.on('user:signin', function(data, answer) {
+    log("User signin: " + JSON.stringify(data));
+    // TODO: Should fetch/generate and return a jwt.
+    answer({status : 0, jwt : "Made up JWT"});
+  });
 });
 
-setInterval(function() {
-    r.table('users')
-        .insert({
-            userId : 'stupiduser' + Date.now(),
-            name : 'whatever',
-            password : 'badpassword'
-        })
-        .run()
-        .then(function(result) {
-            if (result.errors) {
-                log("Error creating user: " + JSON.stringify(result));
-            } else {
-                log("New user created: " + JSON.stringify(result));
-            }
-        });
-}, 1000);
+log("Listening for connections at port " + config.express.port);
 server.listen(config.express.port);
