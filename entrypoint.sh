c58de121f873a54e5588670260d6cf9a6b89ecc0
Olle Sandberg <olle@b3rg.se>
2018-06-17 15:26:00 +0200

    entrypoint.sh: Run backend/start.sh instead of 'npm start' for starting backend.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@ sed -i -e 's/HTTP_PORT/'"${PORT}"'/g' /etc/nginx/conf.d/default.conf
 sed -i -e 's/APP_HOST/'"${APP_HOST}"'/g' /etc/nginx/conf.d/default.conf
 sed -i -e 's/WS_HOST/'"${WS_HOST}"'/g' /etc/nginx/conf.d/default.conf
 
-npm start &
+
+START_CMD="npm start" ./start.sh &
 exec nginx -g 'daemon off;'
